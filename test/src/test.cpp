a9e572008a4f61da20d0cd567a0218ee7978f253
BaerMitUmlaut <BaerMitUmlaut@users.noreply.github.com>
2017-01-06 12:16:53 +0100

    Added simple test utility

---
+++ test/src/test.cpp
@@ -0,0 +1,33 @@
+// Compile with:
+// gcc test.cpp -lstdc++ -Wl,--no-as-needed -ldl -o ../test
+
+#include <iostream>
+#include <dlfcn.h>
+
+int main(int argc, const char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <input>" << std::endl;
+        return 0;
+    }
+
+    void* handle = dlopen("./soil.so", RTLD_LAZY);
+    if (handle == NULL) {
+        std::cerr << "Error: Could not load soil.so." << std::endl;
+        return 0;
+    } else {
+        typedef void (*func_ptr_t)(char*, int, const char*);
+        func_ptr_t rvEntry = (func_ptr_t) dlsym(handle, "RVExtension");
+
+        if (rvEntry == NULL) {
+            std::cerr << "Error: Could not load RVExtension function." << std::endl;
+            return 0;
+        } else {
+            char outputBuffer[10000];
+            rvEntry(outputBuffer, 10000, argv[1]);
+            std::cout << outputBuffer << std::endl;
+        }
+
+        dlclose(handle);
+    }
+    return 0;
+}
